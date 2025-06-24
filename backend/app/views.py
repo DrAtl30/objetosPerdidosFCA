67070cb94fafbc4d9fd0bfab18768e48321fb3dc
ElJeroGGs <josselcrack@gmail.com>
2025-06-23 18:18:18 -0600

    Se unificaron estilos y las plantillas html se separaron en carpetas para meyor orden

--- backend/app/views.py
+++ backend/app/views.py
@@ -5,10 +5,10 @@ from django.http import HttpResponse
 def inicio_sesion(request):
     return render(request, 'inicio_sesion.html')
 def inicio(request):
-    return render(request, 'inicio.html')
+    return render(request, 'users/inicio.html')
 def administrador(request):
-    return render(request, 'administrador.html')
+    return render(request, 'admin/administrador.html')
 def registro_sesion(request):
-    return render(request, 'registro_sesion.html')
+    return render(request, 'users/registro_sesion.html')
 def registro_objeto(request):
-    return render(request, 'registro_objeto.html')
+    return render(request, 'admin/registro_objeto.html')
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
-from app.views import *  # Importar las vistas de la aplicación
+from app.views import *  # Importar las vistasde la aplicación
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    
     path('inicio-sesion', inicio_sesion, name='inicio_sesion'),
     path('inicio', inicio, name='inicio'),
     path('administrador', administrador, name='administrador'),
